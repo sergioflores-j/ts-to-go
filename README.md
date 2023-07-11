c1415182750e427ba11cd29984e48a4ac9abfcc7
Flores Junior, S.E. (Sergio) <sergio.flores@nn.nl>
2023-07-11 10:33:26 +0200

    initial commit

--- README.md
+++ README.md
@@ -1,1 +1,50 @@
 # ts-to-go
+
+Utilities to go for coding Typescript in Golang style!
+
+## Getting started
+
+- Installation
+
+```
+npm i ts-to-go
+
+yarn add ts-to-go
+```
+
+### Wrapping Async functions
+
+```ts
+import { wrapException } from 'ts-to-go';
+
+const wrappedFn = wrapException(async (param1: string) => {
+  if (param1 === 'bar') throw new Error('bar');
+
+  return await Promise.resolve('foo');
+});
+
+const [error, result] = await wrappedFn('bar');
+
+// error: unknown
+// result: string | undefined
+
+if (error instanceof Error) {
+  console.log('handling the error')
+}
+
+console.log('bar', error, result); // bar bar undefined
+```
+
+### Wrapping non Async functions
+
+```ts
+export const sum = wrapException((num1: number, num2: number): number | undefined => {
+  if (num1 === 0) throw new Error('Error!!');
+
+  return num1 + num2;
+});
+
+const [errorS, resultS] = sum(1, 1); // types errorS: unknown, resultS: number | undefined
+
+console.log('sum', errorS, resultS); // sum undefined 2
+```
---
+++ src/index.ts
@@ -0,0 +1,74 @@
+export type PromiseReturningFn<Args extends unknown[], Result = unknown> = (
+  ...args: Args
+) => Promise<Result>;
+
+export type NonPromiseReturningFn<Args extends unknown[], Result = unknown> = (
+  ...args: Args
+) => Result;
+
+export type WrappedResponse<Result = unknown> =
+  | readonly [undefined, Result]
+  | readonly [unknown, undefined];
+
+
+/**
+ * Type guard to check whether the received function is a promise returning function
+ */
+function isPromiseReturningFn<Args extends unknown[], Result>(
+  fn: PromiseReturningFn<Args, Result> | NonPromiseReturningFn<Args, Result>
+): fn is PromiseReturningFn<Args, Result> {
+  return fn[Symbol.toStringTag] === 'AsyncFunction' || fn[Symbol.toStringTag] === 'Promise';
+}
+
+// Overloads
+export function wrapException<Args extends unknown[], Response>(fn: PromiseReturningFn<Args, Response>): ((...args: Args) => Promise<WrappedResponse<Response>>);
+export function wrapException<Args extends unknown[], Response>(fn: NonPromiseReturningFn<Args, Response>): ((...args: Args) => WrappedResponse<Response>);
+
+// Implementation
+/**
+ */
+/**
+ * Wraps a function that returns a Promise or not
+ * Returns a comprehensible tuple interface for better error handling
+ * @returns [Error, Result]
+ * @example
+  const wrappedFn = wrapException(async (param1: string) => {
+    if (param1 === 'bar') throw new Error('bar');
+
+    return await Promise.resolve('foo');
+  });
+
+  // wrappedFn will be typed with `const wrappedFn: (param1: string) => Promise<WrappedResponse<string>>`
+
+  const [error, result] = await wrappedFn('bar');
+
+  // error: unknown
+  // result: string | undefined
+
+  console.log('bar', error, result); // bar bar undefined
+  */
+export function wrapException<Args extends unknown[], Response>(
+  fn: PromiseReturningFn<Args, Response> | NonPromiseReturningFn<Args, Response>
+): ((...args: Args) => Promise<WrappedResponse<Response>> | WrappedResponse<Response>) {
+  // Handles async functions
+  if (isPromiseReturningFn(fn)) {
+    return async (...args: Args): Promise<WrappedResponse<Response>> => {
+      try {
+        const result = await fn(...args);
+        return [undefined, result] as const;
+      } catch (error) {
+        return [error, undefined] as const;
+      }
+    }
+  }
+
+  return (...args: Args): WrappedResponse<Response> => {
+    // Handles sync functions
+    try {
+      const result = fn(...args);
+      return [undefined, result] as const;
+    } catch (error: unknown) {
+      return [error, undefined] as const;
+    }
+  };
+};
